8e2bc502036abc0756063a3ad2e0c6dc5876d3b6
Maksim Momcilovic <M.Momcilovic.inf20@stud.bbbaden.ch>
2022-12-03 19:20:29 +0100

    Update Form.js

--- src/includes/Form.js
+++ src/includes/Form.js
@@ -5,18 +5,19 @@ import Form from "react-bootstrap/Form";
 export default function form() {
   return (
     <div>
-      <form action="/success" method="PUSH" data-netlify="true" onSubmit="submit" data-netlify-honeypot="bot-field">
+      <form name="contact v2" action="/success" method="post" data-netlify="true" onSubmit="submit" data-netlify-honeypot="bot-field">
+        <input type="hidden" name="form-name" value="contact v2"></input>
         <div className="row pt-3">
           <div className="col-lg-6">
             <Form.Group>
               <Form.Label>Name</Form.Label>
-              <Form.Control placeholder="Mustermann" type="text"></Form.Control>
+              <Form.Control value="contact v2" name="name" placeholder="Mustermann" type="text"></Form.Control>
             </Form.Group>
           </div>
           <div className="col-lg-6">
             <Form.Group>
               <Form.Label>Vorname</Form.Label>
-              <Form.Control placeholder="Max" type="text"></Form.Control>
+              <Form.Control value="contact v2" name="vorname" placeholder="Max" type="text"></Form.Control>
             </Form.Group>
           </div>
         </div>
@@ -25,6 +26,7 @@ export default function form() {
           <Form.Group>
             <Form.Label>Telefon</Form.Label>
             <Form.Control
+              value="contact v2" name="telefon"
               placeholder="+ (XX) (XX)(XXX)(XXX)"
               type="tel"
             ></Form.Control>
@@ -33,13 +35,14 @@ export default function form() {
         <div className="col-lg-12 pt-3">
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email-Adresse</Form.Label>
-            <Form.Control type="email" placeholder="example@example.com" />
+            <Form.Control value="contact v2" name="email" type="email" placeholder="example@example.com" />
           </Form.Group>
         </div>
         <div className="col-lg-12">
           <Form.Group className="mb-3" controlId="formBasictxtArea">
             <label htmlFor="message" >Nachricht</label>
             <textarea
+              value="contact v2"
               className="form-control"
               name="message"
               id="message"
@@ -48,7 +51,7 @@ export default function form() {
         </div>
         <div className="row">
           <div className="col-lg-12 text-end">
-            <Button id="submit-btn" type="submit">
+            <Button value="contact v2" id="submit-btn" type="submit">
               Senden
             </Button>
           </div>
