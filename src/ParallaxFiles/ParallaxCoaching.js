6cc89752756025e5ace8e80c3944cd906f23b57f
Maksim Momcilovic <M.Momcilovic.inf20@stud.bbbaden.ch>
2022-12-05 10:48:00 +0100

    .

--- src/ParallaxFiles/ParallaxCoaching.js
+++ src/ParallaxFiles/ParallaxCoaching.js
@@ -8,7 +8,6 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 export default function ParallaxCoaching() {
-  
   let navigate = useNavigate();
   const routeChange = () => {
     let path = `/`;
@@ -25,16 +24,16 @@ export default function ParallaxCoaching() {
     >
       <div className="coaching-content-container-wrapper">
         <div className="coaching-content-container">
-            <div className="back_btn">
-              <button type="button" onClick={routeChange}>
-                &lt; Zurück
-              </button>
-            </div>
+          <div className="back_btn">
+            <button type="button" onClick={routeChange}>
+              &lt; Zurück
+            </button>
+          </div>
           <div className="coaching-container">
             <h1>Persönliches Gespräch</h1>
             <p className="description_coaching">
-              We will be going into depth on exactly what you need help with to
-              achi...
+              Wir werden gemeinsam in die Tiefe tauchen um genau das zu schaffen
+              was du erreichen willst.
             </p>
             <p className="description_details">1 hr | Preis auf Anfrage</p>
             <img
@@ -43,14 +42,16 @@ export default function ParallaxCoaching() {
               src={image}
             ></img>
             <p>
-              I will be breaking things down for you on a full 1 on 1 session!
+              In einem individuellen Gespräch mit dir, bringen wir dein Mindset
+              gemeinsam auf das nächste Level!
               <br />
-              We can talk about anything you need help or work on to achieve
-              your greatest results!
+              Egal was, wir werden es zusammen angehen.
             </p>
             <div id="landcdetails">
               <h2>Genauere Informationen</h2>
-              <a href="mailto:thesoulchannel.web@gmail.com"><p id="mail">thesoulchannel.web@gmail.com</p></a>
+              <a href="mailto:thesoulchannel.web@gmail.com">
+                <p id="mail">thesoulchannel.web@gmail.com</p>
+              </a>
             </div>
             <Contact />
           </div>
