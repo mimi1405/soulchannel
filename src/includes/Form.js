563f5f00d8ca6b88cdc07e7b957172445b85bbf3
Maksim Momcilovic <M.Momcilovic.inf20@stud.bbbaden.ch>
2022-12-05 11:23:17 +0100

    gatsby

--- src/includes/Form.js
+++ src/includes/Form.js
@@ -1,11 +1,37 @@
+/* eslint-disable no-restricted-globals */
+
 import React from "react";
-import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { navigate } from "gatsby";
+
+
+function encode(data) {
+  return Object.keys(data)
+    .map(
+      (key) =>
+        encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+    )
+    .join("&");
+}
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: encode({
+      "form-name": event.target.getAttribute("name"),
+      ...name,
+    }),
+  })
+    .then(() => navigate("/success"))
+    .catch((error) => alert(error));
+};
 
 export default function form() {
   return (
     <div>
-      <form netlify name="contact v2" action="functionallity/success/" method="post" data-netlify="true" onSubmit="submit" data-netlify-honeypot="bot-field">
+      <form netlify name="contact v2" action="functionallity/success/" method="post" data-netlify="true" onSubmit={handleSubmit} data-netlify-honeypot="bot-field">
         <input type="hidden" name="form-name" value="contact v2"></input>
         <div className="row pt-3">
           <div className="col-lg-6">
@@ -50,9 +76,9 @@ export default function form() {
         </div>
         <div className="row">
           <div className="col-lg-12 text-end">
-            <Button value="contact v2" id="submit-btn" type="submit">
+            <button value="contact v2" id="submit-btn" type="submit">
               Senden
-            </Button>
+            </button>
           </div>
         </div>
       </form>
