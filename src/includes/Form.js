a594c547504ed1143d35b84214343390c0f31027
Maksim Momcilovic <M.Momcilovic.inf20@stud.bbbaden.ch>
2022-12-06 07:44:54 +0100

    .

--- src/includes/Form.js
+++ src/includes/Form.js
@@ -2,36 +2,11 @@
 
 import React from "react";
 import Form from "react-bootstrap/Form";
-import { navigate } from "gatsby";
-
-
-function encode(data) {
-  return Object.keys(data)
-    .map(
-      (key) =>
-        encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-    )
-    .join("&");
-}
-
-const handleSubmit = (event) => {
-  event.preventDefault();
-  fetch("/", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: encode({
-      "form-name": event.target.getAttribute("name"),
-      ...name,
-    }),
-  })
-    .then(() => navigate("/success"))
-    .catch((error) => alert(error));
-};
 
 export default function form() {
   return (
     <div>
-      <form netlify name="contact v2" action="functionallity/success/" method="post" data-netlify="true" onSubmit={handleSubmit} data-netlify-honeypot="bot-field">
+      <form netlify name="contact v2" action="functionallity/success/" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
         <input type="hidden" name="form-name" value="contact v2"></input>
         <div className="row pt-3">
           <div className="col-lg-6">
